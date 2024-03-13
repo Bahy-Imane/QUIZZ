2cbc6e0e397c2fc01ddc0b8e50dddbdde97a84c5
I M A N E <imanebahy91@gmail.com>
2024-03-13 15:35:39 +0000

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const  strtbtn = document.querySelector('.head1');
-const btngroup = document.querySelector('.btn-group')
+const  strtbtn = document.querySelector('.butt1');
+const btngroup = document.querySelector('.btn-group');
+const btnExit = document.querySelector('.btn-ext');
+const SEC1 = document.querySelector('.SEC1');
 
 strtbtn.onclick = () => {
     btngroup.classList.add('active');
+    SEC1.classList.add('active');
+}
+
+btnExit.onclick= () => {
+    btngroup.classList.remove('active');
+    SEC1.classList.remove('active');
 }
